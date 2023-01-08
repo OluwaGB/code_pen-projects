a3af6e8cedd025bfe5e432f8f9c42dfa361ee730
I T_code <oluwagbotemioyeboade@gmail.com>
2023-01-08 22:34:37 +0100

    Create Sololearn matrix.js
    The html snippet for the matrix

---
+++ Sololearn matrix.js
@@ -0,0 +1,18 @@
+<!-- Created by Christian D. Fernández -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MATRIX</title>
+
+</head>
+<body>
+	<div id="textC">
+		<h1>SoloLEARN  - MATRIX</h1>
+		<p>Decoding: </p>
+	</div>
+	<div id="textM">
+		
+	</div>
+</body>
+</html>
