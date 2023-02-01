6710dd6d025dad860a05e92073379b1cd6106f32
I T_code <oluwagbotemioyeboade@gmail.com>
2023-02-01 03:14:11 +0100

    Web clone JavaScript code
    Different method of selecting an html element was used in this js file.
    The :
    document.getElementById()
    document.getElementsByClassName()
    document.getElementsByTagName().
    Also different event were used as function in the js file to enable the functionality of the website whenever the user makes the event occur.

---
+++ Web clone.js
@@ -0,0 +1,29 @@
+console.log('Hello World!');
+window.onload = function display(){
+  var arr = document.getElementsByTagName('a');
+  for(i=0; i<=arr.length; i++){
+    arr[0].href= "https://code.sololearn.com/WXuHWM4GrwtK/?ref=app";
+    arr[1].href ="https://code.sololearn.com/W1v6f0h0T4vQ/?ref=app"
+    arr[2].href =" https://code.sololearn.com/W4W5owfIXSoy/?ref=app "
+    arr[3].href =" https://code.sololearn.com/WIsMG9omLzLS/?ref=app"
+    arr[4].href =" https://code.sololearn.com/WSIfBG7r7LHQ/?ref=app "
+    
+  }
+}
+ function link(){var el =
+document.getElementsByTagName("i");
+for (i=0; i<el.length; i++){el[0].href = "https://www.facebook.com/oyeboade.iyanuoluwa"
+el[4].href =" https://github.com/OluwaGB "
+  el[5].href = "https://www.linkedin.com/in/iyanuoluwa-oyeboade-59729a25a"
+  
+}}
+function load(){
+  btn = document.getElementById("body");
+  btn.style.backgroundImage="url('Images/IT.jpeg')";
+  btn = document.getElementById("text")
+  btn.style.color="purple";
+  btn.innerHTML = "I am a frontend developer undergoing brooding in the tech world ,have taken the basic front end courses like html css and JavaScript and had some of my project in codepen and as well as GitHub. I can only participate in self project and not client projects due to me not having access to phone. Getting this system will launch the beginning of my job landing"}
+  function appear(){
+  element =  document.getElementById("form");
+  element.style.display="block";
+  }
